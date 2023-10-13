4f039e96c338961d15fdc37f67740fdc9957d4cd
JonathanWekesser <jowek.jw@gmail.com>
2023-10-13 16:37:05 +0200

    Scanner funktioniert

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Us</h1>
+    <p>
+      Bei Fragen kontaktieren Sie uns, unter:
+      <ul>
+        <li>
+          Emircan Tutar: <a href="mailto:emircan.tutar@rwu.de">emircan.tutar@rwu.de</a>
+        </li>
+        <li>
+          Julian Hellstern: <a href="mailto:julian.hellstern@rwu.de">julian.hellstern@rwu.de</a>
+        </li>
+        <li>
+          Jonathan Wekesser: <a href="mailto:jonathan.wekesser@rwu.de">jonathan.wekesser@rwu.de</a>
+        </li>
+      </ul>
+    </p>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,4 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-
 import { ref } from "vue";
 import { StreamBarcodeReader } from "vue-barcode-reader";
 
@@ -18,7 +16,6 @@ const onDecode = (text) => {
 
 <template>
   <main>
-    <!--<TheWelcome />-->
     <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
     <h2>The decoded value in QR/barcode is</h2>
     <h2>{{ decodedText }}</h2>
