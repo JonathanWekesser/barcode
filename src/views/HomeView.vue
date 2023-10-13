1ccaf4597ed394903ad960aaa361ee34c6e208d2
JonathanWekesser <jowek.jw@gmail.com>
2023-10-13 16:06:13 +0200

    Kamera Test Code kopiert

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,18 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+import { ref } from "vue";
 import { StreamBarcodeReader } from "vue-barcode-reader";
+
+const decodedText = ref("");
+
+const onLoaded = () => {
+  console.log("loaded");
+};
+
+const onDecode = (text) => {
+  decodedText.value = text;
+};
 
 </script>
 
