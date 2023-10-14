85196c8d0e75b9f395421992c60d85ee2d0ec370
Emircan Tutar <emircan.tutar@hs-weingarten.de>
2023-10-14 11:30:14 +0200

    Components und router angepasst

---
+++ src/components/History.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="history">
+    <h1>About Us</h1>
+    <p>
+      Bei Fragen kontaktieren Sie uns, unter:
+      <ul>
+        <li>
+          Emircan Tutar: <a href="mailto:emircan.tutar@rwu.de">emircan.tutar@rwu.de</a>
+        </li>
+        <li>
+          Julian Hellstern: <a href="mailto:julian.hellstern@rwu.de">julian.hellstern@rwu.de</a>
+        </li>
+        <li>
+          Jonathan Wekesser: <a href="mailto:jonathan.wekesser@rwu.de">jonathan.wekesser@rwu.de</a>
+        </li>
+      </ul>
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
